36ffa0573c6e098f0f1465be99412f8774fc37ed
Akanksha-Bathla <160498451+Akanksha-Bathla@users.noreply.github.com>
2026-02-21 18:18:22 +0530

    Time: 27 ms (23.88%), Space: 8 MB (61.81%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -4,7 +4,6 @@ private:
         if(n == 1) return false;
         for(int i=2; i*i<=n; i++){
             if(n%i==0) return false;
-            cout << "Prime" <<i << " ";
         }
         return true;
     }
