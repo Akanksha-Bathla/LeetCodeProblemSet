9c81a95c28b30b28e1cf9eeddc62ab76fdb39460
Akanksha-Bathla <160498451+Akanksha-Bathla@users.noreply.github.com>
2026-03-07 20:06:42 +0530

    Time: 13 ms (75.2%), Space: 14.3 MB (93.6%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -3,12 +3,6 @@ public:
     int minFlips(string s) {
         int n = s.length();
 
-        string s1, s2; //"010101"   s2 //"1010101"
-        for(int i=0; i<2*n; i++){
-            s1 += (i%2) ? '0' : '1';
-            s2 += (i%2) ? '1' : '0';
-        }
-
         //sliding window 
         int flip1 = 0;
         int flip2 = 0;
@@ -16,12 +10,19 @@ public:
 
         int i=0, j=0;
         while(j < 2*n){
-            if(s[j%n] != s1[j]) flip1++;
-            if(s[j%n] != s2[j]) flip2++;
+            char expectedS1 = (j%2)? '0' : '1';
+            char expectedS2 = (j%2)? '1' : '0';
+
+
+            if(s[j%n] != expectedS1) flip1++;
+            if(s[j%n] != expectedS2) flip2++;
 
             if(j-i+1 > n){ //shrink the window from left 
-                if(s[i%n] != s1[i]) flip1--;
-                if(s[i%n] != s2[i]) flip2--;
+                expectedS1 = (i%2)? '0' : '1';
+                expectedS2 = (i%2)? '1' : '0';
+
+                if(s[i%n] != expectedS1) flip1--;
+                if(s[i%n] != expectedS2) flip2--;
 
                 i++;
             }
