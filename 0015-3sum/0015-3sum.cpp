715e2e217a59701422836f5bd96095a01198cf94
Akanksha-Bathla <160498451+Akanksha-Bathla@users.noreply.github.com>
2026-06-17 09:15:01 +0530

    Time: 42 ms (86.53%), Space: 29.1 MB (72.69%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -18,8 +18,8 @@ public:
                     ans.push_back({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
-                    while(j < n && nums[j] == nums[j-1]) j++;
-                    while(k > 0 && nums[k] == nums[k+1]) k--;
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                    while(k > j && nums[k] == nums[k+1]) k--;
                 }
             }
         }
