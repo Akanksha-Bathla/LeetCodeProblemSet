f3a2c4ad7c6da72b38cd17c9b5e95846bafdcafd
Akanksha-Bathla <160498451+Akanksha-Bathla@users.noreply.github.com>
2026-06-23 17:20:40 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -8,13 +8,18 @@ private:
     }
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        dp[0] = 1;
-        dp[1] = 1;
+        // vector<int> dp(n+1, -1);
+        // dp[0] = 1;
+        // dp[1] = 1;
+        int prev2 = 1; //0th stair
+        int prev = 1; // 1st stair
 
         for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            // dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
         }
-        return dp[n];
+        return prev;
     }
 };
