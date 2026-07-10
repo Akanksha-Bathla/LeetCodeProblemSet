28fbe9e9194537ad1480ea23bc9c7c2d62df6094
Akanksha-Bathla <160498451+Akanksha-Bathla@users.noreply.github.com>
2026-07-10 14:41:30 +0530

    Time: 40 ms (29.35%), Space: 27.6 MB (30.84%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,10 +1,10 @@
 class Solution {
 private:
     int f(int i, int j, string& s1, string& s2, vector<vector<int>>& dp){
-        if(i<0 || j<0) return 0;
+        if(i==0 || j==0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
 
-        if(s1[i] == s2[j]) return dp[i][j] = 1 + f(i-1, j-1, s1, s2, dp);
+        if(s1[i-1] == s2[j-1]) return dp[i][j] = 1 + f(i-1, j-1, s1, s2, dp);
 
         return dp[i][j] = max(f(i-1, j, s1, s2, dp), f(i, j-1, s1, s2, dp)); 
     }
@@ -13,7 +13,7 @@ public:
         int n = text1.size();
         int m = text2.size();
 
-        vector<vector<int>>dp (n, vector<int>(m, -1));
-        return f(n-1, m-1, text1, text2, dp);
+        vector<vector<int>>dp (n+1, vector<int>(m+1, -1));
+        return f(n, m, text1, text2, dp);
     }
 };
