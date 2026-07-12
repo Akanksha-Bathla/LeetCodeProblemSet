07faee8a6d75ff710b800cb4c1d38645470f4373
Akanksha-Bathla <160498451+Akanksha-Bathla@users.noreply.github.com>
2026-07-12 08:57:08 +0530

    Time: 24 ms (73.04%), Space: 44.2 MB (32.27%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    int f(int i, int j, string& s, string& t){
+        if(j==0) return 1;
+        if(i==0) return 0;
+
+        if(s[i-1] == t[j-1]) return f(i-1, j-1, s, t) + f(i-1, j, s, t);
+        else return f(i-1, j, s, t);
+    }
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+
+        vector<vector<long long>> dp(n+1, vector<long long>(m+1, 0));
+        for(int i=0; i<=n; i++) dp[i][0] = 1;
+        for(int j=1; j<=m; j++) dp[0][j] = 0;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=min(m, i); j++){
+                if(s[i-1] == t[j-1]) dp[i][j] = ((dp[i-1][j-1] + dp[i-1][j]) >= INT_MAX)? INT_MAX : dp[i-1][j-1] + dp[i-1][j];
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+
+        return dp[n][m];
+    }
+};
