6356e47ae8c16d9947d86b4c0eda65674e504a3f
Akanksha-Bathla <160498451+Akanksha-Bathla@users.noreply.github.com>
2026-07-12 09:30:56 +0530

    Time: 11 ms (87.64%), Space: 8.7 MB (99.19%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -19,12 +19,12 @@ public:
         // for(int j=1; j<=m; j++) dp[0][j] = 0;
 
         for(int i=1; i<=n; i++){
-            curr[0] = 1;
-            for(int j=1; j<=m; j++){
-                if(s[i-1] == t[j-1]) curr[j] = (prev[j-1] >= INT_MAX - prev[j])? INT_MAX : prev[j-1] + prev[j];
-                else curr[j] = prev[j];
+            // curr[0] = 1;
+            prev[0] = 1;
+            for(int j=m; j>=1; j--){
+                if(s[i-1] == t[j-1]) prev[j] = (prev[j-1] >= INT_MAX - prev[j])? INT_MAX : prev[j-1] + prev[j];
+                // else curr[j] = prev[j];
             }
-            prev = curr;
         }
 
         return prev[m];
