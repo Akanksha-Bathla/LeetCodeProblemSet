ae051f19ef332da94f8510a89ca4cdbe9c4c97e9
Akanksha-Bathla <160498451+Akanksha-Bathla@users.noreply.github.com>
2026-07-12 09:19:41 +0530

    Time: 15 ms (81.97%), Space: 8.9 MB (95.42%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -12,17 +12,21 @@ public:
         int n = s.size();
         int m = t.size();
 
-        vector<vector<long long>> dp(n+1, vector<long long>(m+1, 0));
-        for(int i=0; i<=n; i++) dp[i][0] = 1;
-        for(int j=1; j<=m; j++) dp[0][j] = 0;
+        // vector<vector<long long>> dp(n+1, vector<long long>(m+1, 0));
+        vector<int> prev(m+1, 0), curr(m+1, 0);
+        // for(int i=0; i<=n; i++) dp[i][0] = 1;
+        prev[0] = 1;
+        // for(int j=1; j<=m; j++) dp[0][j] = 0;
 
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=min(m, i); j++){
-                if(s[i-1] == t[j-1]) dp[i][j] = ((dp[i-1][j-1] + dp[i-1][j]) >= INT_MAX)? INT_MAX : dp[i-1][j-1] + dp[i-1][j];
-                else dp[i][j] = dp[i-1][j];
+            curr[0] = 1;
+            for(int j=1; j<=m; j++){
+                if(s[i-1] == t[j-1]) curr[j] = (prev[j-1] >= INT_MAX - prev[j])? INT_MAX : prev[j-1] + prev[j];
+                else curr[j] = prev[j];
             }
+            prev = curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
