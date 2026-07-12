cfbfff37ca292e5bacff4b1978c279928ec80f44
Akanksha-Bathla <160498451+Akanksha-Bathla@users.noreply.github.com>
2026-07-12 12:25:32 +0530

    Time: 6 ms (68.6%), Space: 13 MB (79.91%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,28 @@
+class Solution {
+// private:
+//     int f(int i, int j, string& s1, string& s2){
+//         if(i<0) return j+1;
+//         if(j<0) return i+1;
+
+//         if(s1[i] == s2[j]) return f(i-1, j-1, s1, s2);
+//         else return 1 + min(f(i,j-1, s1, s2), min(f(i-1, j-1, s1, s2), f(i-1, j, s1, s2)));
+//     }
+public:
+    int minDistance(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+        for(int i=0; i<=n; i++) dp[i][0] = i+1;
+        for(int j=1; j<=m; j++) dp[0][j] = j+1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
+            }
+        }
+
+        return dp[n][m]-1;
+    }
+};
