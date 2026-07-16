3700c09f938cc6f42fd930d23c4e7fc10a57dccd
Akanksha-Bathla <160498451+Akanksha-Bathla@users.noreply.github.com>
2026-07-16 18:48:24 +0530

    Time: 0 ms (100%), Space: 20 MB (84.52%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,36 +1,41 @@
 class Solution {
 private:
-    int f(int ind, int buy, vector<int>& arr, int n, vector<vector<int>> &dp){
-        if(ind == n) return 0;
+    // int f(int ind, int buy, vector<int>& arr, int n, vector<vector<int>> &dp){
+    //     if(ind == n) return 0;
 
-        if(dp[ind][buy] != -1) return dp[ind][buy];
+    //     if(dp[ind][buy] != -1) return dp[ind][buy];
 
-        int profit = 0;
-        if(buy){
-            profit = max(-arr[ind] + f(ind+1, 0, arr, n, dp), f(ind+1, 1, arr, n, dp));
-        }
-        else{
-            profit = max(arr[ind] + f(ind+1, 1, arr, n, dp), f(ind+1, 0, arr, n, dp));
-        }
+    //     int profit = 0;
+    //     if(buy){
+    //         profit = max(-arr[ind] + f(ind+1, 0, arr, n, dp), f(ind+1, 1, arr, n, dp));
+    //     }
+    //     else{
+    //         profit = max(arr[ind] + f(ind+1, 1, arr, n, dp), f(ind+1, 0, arr, n, dp));
+    //     }
 
-        return dp[ind][buy] = profit;
-    }
+    //     return dp[ind][buy] = profit;
+    // }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
 
-        vector<vector<int>> dp(n+1, vector<int>(2, 0));
-        dp[n][0] = dp[n][1] = 0;
+        // vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        // dp[n][0] = dp[n][1] = 0;
 
-        for(int i=n-1; i>=0; i--){
-            for(int b=0; b<2; b++){
-                int profit = 0;
-                if(b) profit = max(-prices[i] + dp[i+1][0], dp[i+1][1]);
-                else profit = max(prices[i] + dp[i+1][1], dp[i+1][0]);
-                dp[i][b] = profit;
-            }
+        // for(int i=n-1; i>=0; i--){
+        //     for(int b=0; b<2; b++){
+        //         int profit = 0;
+        //         if(b) profit = max(-prices[i] + dp[i+1][0], dp[i+1][1]);
+        //         else profit = max(prices[i] + dp[i+1][1], dp[i+1][0]);
+        //         dp[i][b] = profit;
+        //     }
+        // }
+
+        // return dp[0][1];
+        int profit = 0;
+        for(int i=1; i<n; i++){
+            profit += max(0, prices[i] - prices[i-1]);
         }
-
-        return dp[0][1];
+        return profit;
     }
 };
