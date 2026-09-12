6a3d0cdbed9ce9ed93c696781eac905fc59e7279
Akanksha-Bathla <160498451+Akanksha-Bathla@users.noreply.github.com>
2026-09-12 17:45:45 +0530

    Time: 0 ms (100%), Space: 10.2 MB (97.12%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,16 +1,20 @@
 class Solution {
 private:
-    void f(int i, vector<int>& arr, vector<int>& nums, set<vector<int>>& result){
-        if(i == nums.size()){
-            result.insert(arr);
-            return;
+    void f(int i, vector<int>& arr, vector<int>& nums, vector<vector<int>>& result){
+        result.push_back(arr);
+
+        for(int j = i; j < nums.size(); j++){
+            if(j>i && nums[j] == nums[j-1]) continue;
+
+            arr.push_back(nums[j]);
+            f(j+1, arr, nums, result); // take
+            arr.pop_back();
         }
+        // f(i+1, arr, nums, result); //notTake
 
-        f(i+1, arr, nums, result); //notTake
-
-        arr.push_back(nums[i]);
-        f(i+1, arr, nums, result); // take
-        arr.pop_back();
+        // arr.push_back(nums[i]);
+        // f(i+1, arr, nums, result); // take
+        // arr.pop_back();
 
         return;
     }
@@ -18,13 +22,13 @@ public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
 
-        set<vector<int>> result;
+        // set<vector<int>> result;
         vector<int> arr;
 
         sort(nums.begin(), nums.end());
 
-        f(0, arr, nums, result);
-        ans.assign(result.begin(), result.end());
+        f(0, arr, nums, ans);
+        // ans.assign(result.begin(), result.end());
         return ans;
     }
 };
